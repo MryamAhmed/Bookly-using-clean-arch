2110c721d1ea7a1fe8bccc22bf7c2776dcdad406
MryamAhmed <mrym8499@gmail.com>
2024-02-02 16:25:18 +0200

    best seller

---
+++ lib/features/home/views/widget/best_seller_list_view_item.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/core/extention/spacing.dart';
+
+import '../../../../core/utils/assets.dart';
+import '../../../../core/utils/styles.dart';
+import 'book_ratting.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: SizedBox(
+        height: 120,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 1.6 / 2,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.red,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        AssetsData.testImage,
+                      ),
+                    )),
+              ),
+            ),
+            30.horizontal,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .5,
+                    child: Text('title',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Styles.textStyle20.copyWith(//fontFamily: ),
+                            )),
+                  ),
+                  3.vertical,
+                  const Text('author'),
+                  3.vertical,
+                  Row(
+                    children: [
+                      Text(
+                        'free',
+                        style: Styles.textStyle20
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      const BookRatting(),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/home/views/widget/book_ratting.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../core/utils/styles.dart';
+
+class BookRatting extends StatelessWidget {
+  const BookRatting({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Icon(
+          FontAwesomeIcons.solidStar,
+          color: Color(0xffFFDD4F),
+          size: 14,
+        ),
+        const SizedBox(
+          width: 6.3,
+        ),
+        const Text(
+          '4.8',
+          style: Styles.textStyle16,
+        ),
+        const SizedBox(
+          width: 5,
+        ),
+        Opacity(
+          opacity: .5,
+          child: Text(
+            '(245)',
+            style: Styles.textStyle14.copyWith(
+                color: const Color(0xff707070), fontWeight: FontWeight.w600),
+          ),
+        ),
+      ],
+    );
+  }
+}
